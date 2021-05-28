130ece5b420bf1850ca2279c9fb6f9c75f20024e
Mike Skotar <michaelskotar@gmail.com>
2021-05-28 12:58:06 -0400

    Fixed Styling and sizing

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 700,
     webPreferences:{
       nodeIntegration: true,
       enableRemoteModule: true
